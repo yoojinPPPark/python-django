257790bf8e4c92af594f93087501d67e1212f1e7
Henry-Hong <hhr8540@naver.com>
2022-08-11 17:36:15 +0900

    테스트

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, test2, test3
 
 urlpatterns = [
     path('', index, name='index'),
+    path('test2/', test2, name='test2'),
+    path('test3/', test3, name='test3'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, JsonResponse, redirect
+
+# def index(request):
+# 	return render(request, 'core/index.html')
 
 def index(request):
-	return render(request, 'core/index.html')
+    return HttpResponse("http response 테스트")
+
+def test2(request):
+  return JsonResponse("json response 테스트")
+
+json = {
+  "a": "1",
+  "b": "2",
+  "c": "3",
+}
+
+def test3(request):
+  return JsonResponse(json)
